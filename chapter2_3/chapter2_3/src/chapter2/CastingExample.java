89ab5264eca0cbfeb5010ba65cc3cb68b87f668e
wanwan2 <121847253+wanwanwan2@users.noreply.github.com>
2023-11-14 01:23:45 +0900

    자동 타입 변환, 강제 타입 변환, 정수 타입의 연산(byte, long) 등
    추가 내용
    연산식에서 자동 타입 변환
    문자열 결합 연산
    기본 타입과 문자열 간의 변환

---
+++ chapter2_3/chapter2_3/src/chapter2/CastingExample.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class CastingExample {
+
+	public static void main(String[] args) {
+		//강제 타입 변환
+	
+		int intValue = 44032;
+		char charValue = (char) intValue;
+		System.out.println(charValue);
+		
+		long longValue = 500;
+		intValue = (int) longValue;
+		System.out.println(intValue);
+		
+		double doubleValue = 3.14;
+		intValue = (int) doubleValue;
+		System.out.println(intValue);
+	}
+
+}
