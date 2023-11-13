97b2bb0ab38cf879895d96e96c9bf362358cb2e4
wanwan2 <121847253+wanwanwan2@users.noreply.github.com>
2023-11-14 01:18:14 +0900

    byte 타입 변수, long 타입 변수, char 타입, String 타입 등
    50자 글자 제한 있어서 위 제목에 못 적은 내용들입니다.
    추가로 설명드리면 이스케이프 문자 출력, float과 double 타입, boolean 타입까지 있습니다.

---
+++ chapter2_2/chapter2_2/src/chapter2/BooleanExample.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		
+		boolean stop = true;
+		if(stop) {
+			System.out.println("중지합니다.");
+		} else {
+			System.out.println("시작합니다.");
+		}
+
+	}
+
+}
