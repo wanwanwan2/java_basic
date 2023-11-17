5c86593b05d804c39f5e7cadc7ae19d36077fefc
WanPatch <121847253+wanwanwan2@users.noreply.github.com>
2023-11-18 01:17:11 +0900

    인스턴스 멤버와 정적 멤버
    인스턴스 멤버와 this
    정적 멤버 사용
    정적 메소드 선언 시 주의할 점
    싱글톤 
    싱글톤 객체
    final 필드 선언과 초기화
    final 필드 테스트
    상수 선언
    상수 사용

---
+++ chapter6_5/chapter6_5/src/chapter6/Car2.java
@@ -0,0 +1,15 @@
+package chapter6;
+
+public class Car2 {
+	int speed;
+	
+	void run() {
+		System.out.println(speed + "으로 달립니다.");
+	}
+	
+	public static void main(String[] args) {
+		Car2 myCar = new Car2();
+		myCar.speed = 60;
+		myCar.run();
+	}
+}
---
+++ chapter6_5/chapter6_5/src/chapter6/Singleton.java
@@ -0,0 +1,11 @@
+package chapter6;
+
+public class Singleton {
+	private static Singleton singleton = new Singleton();
+	
+	private Singleton() {}
+	
+	static Singleton getInstance() {
+		return singleton;
+	}
+}
