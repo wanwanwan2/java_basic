2773475e38a81a329581df123e040836fa61d5c5
WanPatch <121847253+wanwanwan2@users.noreply.github.com>
2023-11-17 10:26:42 +0900

    열거타입, 열거타입 선언, 열거 타입과 열거 상수

---
+++ chapter5_3/chapter5_2/src/chapter5/Exercise13.java
@@ -0,0 +1,16 @@
+package chapter5;
+
+import java.util.Calendar;
+
+public class Exercise13 {
+
+	public static void main(String[] args) {
+		LoginResult result = LoginResult.FAIL_PASSWARD;
+		if (result == LoginResult.SUCCESS) {
+		} else if (result == LoginResult.FAIL_ID) {
+		} else if (result == LoginResult.FAIL_PASSWARD) {
+		}     
+		System.out.println(result);
+	}
+
+}
---
+++ chapter5_3/chapter5_2/src/chapter5/LoginResult.java
@@ -0,0 +1,7 @@
+package chapter5;
+
+public enum LoginResult {
+	SUCCESS,
+	FAIL_ID,
+	FAIL_PASSWARD
+}
