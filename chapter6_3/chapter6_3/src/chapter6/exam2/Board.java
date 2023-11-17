8c236388ed208a4a20924c8312359ddb05dfda6d
WanPatch <121847253+wanwanwan2@users.noreply.github.com>
2023-11-18 01:12:23 +0900

    생성자
    생성자 선언
    생성자를 호출해서 객체 생성
    생성자에서 필드 초기화
    객체 생성 후 필드 값 출력
    생성자의 오버로딩
    객체 생성 시 생성자 선택
    다른 생성자를 호출해서 중복 코드 줄이기
    객체 생성 시 생성자 선택

---
+++ chapter6_3/chapter6_3/src/chapter6/exam2/Board.java
@@ -0,0 +1,30 @@
+package chapter6.exam2;
+
+public class Board {
+	
+	String title;
+	String content;
+	String writer;
+	String date;
+	int hitcount;
+	 
+	Board(String title, String content) {
+		this(title, content, "로그인한 회원아이디", "현재 컴퓨터 날짜", 0);
+	}
+	
+	Board(String title, String content, String writer) {
+		this(title, content, writer, "현재 컴퓨터 날짜", 0);
+	}
+	
+	Board(String title, String content, String writer, String date) {
+		this(title, content, writer, date, 0);
+	}
+
+	Board(String title2, String content2, String string, String string2, int i) {
+		this.title = title;
+		this.content = content;
+		this.writer = writer;
+		this.date = date;
+		this.hitcount = hitcount;
+	}
+}
---
+++ chapter6_3/chapter6_3/src/chapter6/exam2/Member.java
@@ -0,0 +1,14 @@
+package chapter6.exam2;
+
+public class Member {
+	
+	String name;
+	String id;
+	String password;
+	int age;
+	
+	Member(String name, String id) {
+		this.name = name;
+		this.id = id;
+	}
+}
