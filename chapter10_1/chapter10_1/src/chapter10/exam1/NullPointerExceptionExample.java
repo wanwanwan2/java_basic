72cdbf34a3358ebd7113157d3fb171ad0303ee02
WanPatch <121847253+wanwanwan2@users.noreply.github.com>
2023-11-22 02:17:57 +0900

    예외 클래스
    NullPointerException
    ArrayIndexOutOfBoundsException
    NumberFormatException
    ClassCastException

---
+++ chapter10_1/chapter10_1/src/chapter10/exam1/NullPointerExceptionExample.java
@@ -0,0 +1,10 @@
+package chapter10.exam1;
+
+public class NullPointerExceptionExample {
+
+	public static void main(String[] args) {
+		String data = null;
+		System.out.println(data.toString());    
+	}
+
+}
---
+++ chapter10_1/chapter10_1/src/chapter10/exam5/ClassCastExceptionExample.java
@@ -0,0 +1,22 @@
+package chapter10.exam5;
+
+public class ClassCastExceptionExample {
+
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		changeDog(dog);
+		
+		Cat cat = new Cat();
+		changeDog(cat);
+	}
+	
+	public static void changeDog(Animal animal) {
+//		if (condition) {
+		Dog dog = (Dog) animal;
+//		}
+	}
+}
+
+class Animal {}
+class Dog extends Animal {}
+class Cat extends Animal {}   
