658ad04679b9fb1f539073f4372396a0a69869b6
WanPatch <121847253+wanwanwan2@users.noreply.github.com>
2023-11-24 20:06:09 +0900

    스레드(멀티 스레드)
    메인 스레드만 이용하는 경우
    비프음을 들려주는 작업 정의
    메인스레드와 작업 스레드가 동시에 실행
    비프음을 들려주는 스레드
    메인 스레드 이름 출력 및 UserThread 생성 및 시작

---
+++ chapter12_1/chapter12_1/src/chapter12/exam07/User2.java
@@ -0,0 +1,14 @@
+package chapter12.exam07;
+
+public class User2 extends Thread {
+	private Calculator calculator;
+	
+	public void setCalculator(Calculator calculator) {
+		this.setName("User2");
+		this.calculator = calculator;
+	}
+	
+	public void run() {
+		calculator.setMemory(50);
+	}
+}
