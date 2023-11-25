f271065d2efda9fbb674537d122b7ce572551b8a
WanPatch <121847253+wanwanwan2@users.noreply.github.com>
2023-11-25 18:33:55 +0900

    LIFO 와 FIFO 컬렉션

---
+++ chapter13_2/chapter13_2/src/chapter13/exam01/Coin.java
@@ -0,0 +1,13 @@
+package chapter13.exam01;
+
+public class Coin {
+	private int value;
+	
+	public Coin(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+}
---
+++ chapter13_2/chapter13_2/src/chapter13/exam02/Message.java
@@ -0,0 +1,12 @@
+package chapter13.exam02;
+
+public class Message {
+
+	public String command;
+	public String to;
+	
+	public Message(String command, String to) {
+		this.command = command;
+		this.to = to;
+	}
+}
